e115ac1c6e00aef5b65dcebb30cb0f82dfe3756e
Juliana Karoline <julianaklulo@gmail.com>
2020-11-06 19:22:12 -0300

    Create Product filter

--- products/filters.py
+++ products/filters.py
@@ -4,3 +4,9 @@ from django_filters import rest_framework as filters
 class CategoryFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr="contains")
 
+
+class ProductFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr="contains")
+    description = filters.NumberFilter(lookup_expr="contains")
+    price = filters.NumberFilter(lookup_expr="exact")
+    categories__name = filters.CharFilter(lookup_expr="contains")
