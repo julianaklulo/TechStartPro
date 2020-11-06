8a65d8d30fc014668f27c1c88765ba456bf7967b
Juliana Karoline <julianaklulo@gmail.com>
2020-11-06 19:45:59 -0300

    Create Category viewset for CRUD operations

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import CategorySerializer
+from .models import Category
+from .filters import CategoryFilter
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+    filterset_class = CategoryFilter
 
 # Create your views here.
