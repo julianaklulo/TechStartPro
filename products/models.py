c7a324214a4a2e5c8d6e3e3adf83862b3d0026e2
Juliana Karoline <julianaklulo@gmail.com>
2020-11-06 17:51:39 -0300

    Create Product model

--- products/models.py
+++ products/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
