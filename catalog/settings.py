30f31ae2a68de3e6cacde1dbbba819c09917b756
Juliana Karoline <julianaklulo@gmail.com>
2020-11-06 22:41:58 -0300

    Change settings to load database from env var

--- catalog/settings.py
+++ catalog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import dj_database_url
 import dotenv
 import os
 from pathlib import Path
@@ -81,12 +82,7 @@ WSGI_APPLICATION = 'catalog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
